c7e4cf26b0dee7561a3289ceea71ce07489f3a59
VIJAY33420 <vijay.diwaniya.cg@gmail.com>
2025-09-15 10:29:20 +0530

    Q-8

---
+++ c-language/Q-8.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+   int a;
+   scanf("%d",&a);
+   
+   for(int i=a; i>0; i--){
+       for(int j=1; j<=a-i+1; j++){
+       printf("%d ",j);
+   }printf("\n");
+}
+    return 0;
+}
