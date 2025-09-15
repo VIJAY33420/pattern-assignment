9127c3694e0a86fbd7f0f9b5ff948fd7fac35af3
VIJAY33420 <vijay.diwaniya.cg@gmail.com>
2025-09-15 10:31:11 +0530

    Q-9

---
+++ c-language/Q-9.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+   int a;
+   scanf("%d",&a);
+   
+   for(int i=a; i>0; i--){
+       for(int j=0; j<a-i+1; j++){ 
+       printf("%c ",'A'+j); 
+   }printf("\n");
+}
+    return 0;
+}
