cd7e26b2388756878dc4731f51263ebf3fa6faa4
VIJAY33420 <vijay.diwaniya.cg@gmail.com>
2025-09-15 11:10:13 +0530

    Q-28

---
+++ c-language/Q-28.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int count =0;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<i; j++){
+            printf(" ");
+        }
+        for(int k=0; k<2*(n-i)-1; k++){
+            printf("%c ",'A'+count);
+            count++;
+        } 
+        
+        printf("\n");
+        count=0;
+    }
+
+    return 0;
+}
