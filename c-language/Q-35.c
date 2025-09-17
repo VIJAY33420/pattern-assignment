145e74706d990c97ec4dad4fdc2b6057736d7fbe
VIJAY33420 <vijay.diwaniya.cg@gmail.com>
2025-09-17 14:30:37 +0530

    new

---
+++ c-language/Q-35.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+   int n; 
+   scanf("%d",&n);
+   
+   for(int i=n; i>=0; i--){
+       for(int j=0; j<=(n-i); j++){
+           printf(" ");
+       }
+       for(int k=1; k<=n-i; k++){
+           printf("%d",k);
+       }
+      printf("\n"); 
+   }
+    return 0;
+}
